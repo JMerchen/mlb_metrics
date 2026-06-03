f9a776899000f7bca3e0f3c77023c2a2c5f6aa94
Jackson Merchen <77132736+JMerchen@users.noreply.github.com>
2026-06-02 17:19:54 -0700

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,49 @@
-console.log("APP RUNNING")
+async function loadWave(){
 
 document
 .getElementById(
 "output"
 )
 .innerHTML =
-"javascript loaded"
+"attempting fetch"
+
+try{
+
+const response =
+await fetch(
+"../data/wave.csv"
+)
+
+const text =
+await response.text()
+
+document
+.getElementById(
+"output"
+)
+.innerHTML =
+"<pre>"
++
+text.slice(
+0,
+1500
+)
++
+"</pre>"
+
+}
+
+catch(err){
+
+document
+.getElementById(
+"output"
+)
+.innerHTML =
+err
+
+}
+
+}
+
+loadWave()
