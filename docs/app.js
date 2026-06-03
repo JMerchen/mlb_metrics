8e16d6c5943850f54b65a102e90e97c1042a4b38
Jackson Merchen <77132736+JMerchen@users.noreply.github.com>
2026-06-03 11:32:34 -0700

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,5 +1,7 @@
 async function loadWave(){
 
+try{
+
 const response =
 await fetch(
 "./data/wave.csv"
@@ -8,19 +10,73 @@ await fetch(
 const text =
 await response.text()
 
+const rows =
+text
+.trim()
+.split("\n")
+
+let html =
+"<table>"
+
+rows
+.slice(
+0,
+20
+)
+.forEach(
+(
+row,
+index
+)=>{
+
+const cols =
+row.split(",")
+
+html +=
+"<tr>"
+
+cols.forEach(
+value=>{
+
+html +=
+index===0
+?
+`<th>${value}</th>`
+:
+`<td>${value}</td>`
+
+})
+
+html +=
+"</tr>"
+
+}
+)
+
+html +=
+"</table>"
+
 document
 .getElementById(
 "output"
 )
 .innerHTML =
-"<pre>"
-+
-text.slice(
-0,
-1500
+html
+
+}
+
+catch(err){
+
+document
+.getElementById(
+"output"
 )
-+
-"</pre>"
+.innerHTML =
+"Could not load CSV"
+
+console.log(err)
+
+}
 
 }
 
