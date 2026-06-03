678e726de49627112264d33340407f9310ea6ff4
Jackson Merchen <77132736+JMerchen@users.noreply.github.com>
2026-06-02 17:17:36 -0700

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,55 +1,8 @@
-async function loadWave(){
-
-try{
-
-const response =
-await fetch(
-"./data/wave.csv"
-)
-
-if(!response.ok){
-
-throw new Error(
-response.status
-)
-
-}
-
-const text =
-await response.text()
+console.log("APP RUNNING")
 
 document
 .getElementById(
 "output"
 )
 .innerHTML =
-"<pre>"
-+
-text
-.substring(
-0,
-1500
-)
-+
-"</pre>"
-
-}
-
-catch(err){
-
-document
-.getElementById(
-"output"
-)
-.innerHTML =
-"ERROR:<br><br>"
-+
-err
-
-console.log(err)
-
-}
-
-}
-
-loadWave()
+"javascript loaded"
