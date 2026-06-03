e0c51981ef90dd891ce2345c5c5dc607b097014d
Jackson Merchen <77132736+JMerchen@users.noreply.github.com>
2026-06-02 17:11:47 -0700

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,28 @@
-console.log(
-"site loaded"
+async function loadWave(){
+
+const response =
+await fetch(
+"/mlb_metrics/data/wave.csv"
 )
+
+const text =
+await response.text()
+
+document
+.getElementById(
+"output"
+)
+.innerHTML =
+"<pre>"
++
+text
+.substring(
+0,
+1500
+)
++
+"</pre>"
+
+}
+
+loadWave()
