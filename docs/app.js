337dee8f304aca4df3a4541a37bacd4d31ca3a5d
Jackson Merchen <77132736+JMerchen@users.noreply.github.com>
2026-06-03 11:54:31 -0700

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,215 +1,60 @@
-async function loadCSV(path){
+async function test(){
+
+try{
+
+document
+.getElementById(
+"waveTable"
+)
+.innerHTML =
+"Loading WAVE..."
 
 const response =
-await fetch(path)
+await fetch(
+"./data/wave.csv"
+)
+
+document
+.getElementById(
+"waveTable"
+)
+.innerHTML =
+`
+Status:
+${response.status}
+`
 
 const text =
 await response.text()
 
-const rows =
-text
-.trim()
-.split("\n")
-
-const headers =
-rows[0]
-.split(",")
-
-return rows
-.slice(1)
-.map(row=>{
-
-const values =
-row.split(",")
-
-let obj={}
-
-headers.forEach(
-(h,i)=>
-obj[h]=values[i]
-)
-
-return obj
-
-})
-
-}
-
-function makeTable(
-data,
-container,
-limit=null
-){
-
-if(
-limit
-){
-
-data =
-data.slice(
-0,
-limit
-)
-
-}
-
-if(
-data.length===0
-){
-
 document
 .getElementById(
-container
+"waveTable"
 )
 .innerHTML =
-"No data"
+`
+Loaded
 
-return
-
-}
-
-const cols =
-Object.keys(
-data[0]
-)
-
-let html =
-"<table>"
-
-html +=
-"<tr>"
-
-cols.forEach(
-c=>
-html +=
-`<th>${c}</th>`
-)
-
-html +=
-"</tr>"
-
-data.forEach(
-row=>{
-
-html +=
-"<tr>"
-
-cols.forEach(
-c=>{
-
-html +=
-`<td>${row[c]}</td>`
-
-})
-
-html +=
-"</tr>"
-
-}
-)
-
-html +=
-"</table>"
-
-document
-.getElementById(
-container
-)
-.innerHTML =
-html
-
-}
-
-function loadTeams(
-data
-){
-
-const a =
-document
-.getElementById(
-"teamA"
-)
-
-const b =
-document
-.getElementById(
-"teamB"
-)
-
-data.forEach(
-t=>{
-
-if(
-t.team
-){
-
-a.innerHTML +=
-`<option>
-${t.team}
-</option>`
-
-b.innerHTML +=
-`<option>
-${t.team}
-</option>`
-
-}
-
-}
-)
-
-}
-
-async function loadAll(){
-
-try{
-
-const wave =
-await loadCSV(
-"./data/wave.csv"
-)
-
-const pave =
-await loadCSV(
-"./data/pave.csv"
-)
-
-const confidence =
-await loadCSV(
-"./data/confidence.csv"
-)
-
-makeTable(
-wave,
-"waveTable",
-20
-)
-
-makeTable(
-pave,
-"paveTable",
-20
-)
-
-makeTable(
-confidence,
-"confidenceTable"
-)
-
-loadTeams(
-confidence
-)
+${text.slice(0,300)}
+`
 
 }
 
 catch(err){
 
-console.log(
-err
+document
+.getElementById(
+"waveTable"
 )
+.innerHTML =
+`
+ERROR:
+
+${err}
+`
 
 }
 
 }
 
-loadAll()
+test()
