aba7c7e0b2683d4063964c6c035e3a9649b4d957
Jackson Merchen <77132736+JMerchen@users.noreply.github.com>
2026-06-09 16:48:08 -0700

    Update wave.py

--- scripts/wave.py
+++ scripts/wave.py
@@ -25,6 +25,8 @@ away = df[df['inning_topbot'] == 'Top'][['batter', 'away_team', 'game_date']].dr
 team = pd.concat([home, away])
 latest_player = df[['game_date', 'batter']].groupby('batter', as_index = False).max()
 latest_team = latest_player.merge(team, on = ['batter', 'game_date'], how = 'left')[['batter', 'team']].rename(columns = {'batter' : 'key_mlbam'})
+latest_pitcher = df[['game_date', 'pitcher']].groupby('pitcher', as_index = False).max()
+latest_pitcher_team = latest_pitcher.merge(team, on = ['pitcher', 'game_date'], how = 'left')[['pitcher', 'team']].rename(columns = {'pitcher' : 'key_mlbam'})
 
 """## WAVE"""
 
@@ -272,6 +274,7 @@ pave['Expected_Hits'] = pave['baa'] * 22
 pave['Expected_Bases'] = pave['power_a'] * 22
 pave['Expected_HRs'] = pave['hr_per'] * 22
 pave = pave[['key_mlbam', 'name_first', 'name_last', 'at_bats', 'PAVE_PLUS', 'Expected_Hits', 'Expected_Bases', 'Expected_HRs']]
+pave = pave.merge(latest_pitcher_team, on='key_mlbam', how='left')
 pave = pave.sort_values('PAVE_PLUS', ascending=False)
 
 """# Hit_Prob"""
